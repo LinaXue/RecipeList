8fc2998cc2289f8216af3527233324b6e9598bb9
Lina <zxc789885@gmail.com>
2024-10-25 13:08:22 +0800

    feat: ViewModel 資料處理邏輯更動
    因應 Room table 'recipes' 欄位更動
    修改 RecipeInsertViewModel 處理資料邏輯
    
    調整項目:
    1. data class RecipeDetails 新增 ingredients, step
    2. 新增 ingredientUiState 參數與其 update、delete function
    3. 新增 stepUiState 參數與其 update、delete function
    4. 新增 validate function ，確認 ingredientUiState 與 stepUiState 資料可用性
    5. 修改 insertRecipe data class，將 ingredients, step 也一起保存

--- app/src/main/java/com/demo/recipelist/ui/screen/RecipeEditViewModel.kt
+++ app/src/main/java/com/demo/recipelist/ui/screen/RecipeEditViewModel.kt
@@ -1,6 +1,8 @@
 package com.demo.recipelist.ui.screen
 
+import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
@@ -23,6 +25,12 @@ class RecipeEditViewModel(
     var recipeUiState by mutableStateOf(RecipeUiState())
         private set
 
+    var ingredientUiState = mutableStateListOf("", "")
+        private set
+
+    var stepUiState = mutableStateListOf("", "")
+        private set
+
     private val recipeId: Int = checkNotNull(savedStateHandle[EditDestination.recipeIdArg])
 
     init {
@@ -38,10 +46,27 @@ class RecipeEditViewModel(
         recipeUiState =
             RecipeUiState(recipeDetails = recipeDetails, isInputValid = validateInput(recipeDetails))
     }
+    fun updateIngredientUiState(index: Int, newIngredientUiState: String) {
+        ingredientUiState[index] = newIngredientUiState
+    }
+    fun deleteIngredientUiState(ingredient: String) {
+        ingredientUiState.remove(ingredient)
+    }
+    fun updateStepUiState(index: Int, newStepUiState: String) {
+        stepUiState[index] = newStepUiState
+    }
+    fun deleteStepUiState(step: String) {
+        stepUiState.remove(step)
+    }
 
     suspend fun updateRecipe() {
         if (validateInput(recipeUiState.recipeDetails)) {
-            recipeRepository.updateRecipe(recipeUiState.recipeDetails.toRecipe())
+            val newRecipe = recipeUiState.recipeDetails.toRecipe()
+            Log.d("EditScreen", "recipe: ${recipeUiState.recipeDetails.toRecipe()}")
+            newRecipe.ingredients = validateIngredients()
+            newRecipe.steps = validateSteps()
+            Log.d("InsertScreen", "new recipe: id = ${newRecipe.id}, title = ${newRecipe.title}, servings = ${newRecipe.servings}, ingredients = ${newRecipe.ingredients}, steps = ${newRecipe.steps} \n")
+            // recipeRepository.updateRecipe(newRecipe)
         }
     }
 
@@ -49,5 +74,23 @@ class RecipeEditViewModel(
         return with(uiState) {
             title.isNotBlank() && description.isNotBlank() && time.isNotBlank()
         }
+    }
+    private fun validateIngredients(): String {
+        var ingredientsString = ""
+        for (ing in ingredientUiState) {
+            if (ing.isNotBlank()) {
+                ingredientsString = ingredientsString + ing + "\n"
+            }
+        }
+        return ingredientsString
+    }
+    private fun validateSteps(): String {
+        var stepsString = ""
+        for (step in stepUiState) {
+            if (step.isNotBlank()) {
+                stepsString = stepsString + step + "\n"
+            }
+        }
+        return stepsString
     }
 }
--- app/src/main/java/com/demo/recipelist/ui/screen/RecipeInsertViewModel.kt
+++ app/src/main/java/com/demo/recipelist/ui/screen/RecipeInsertViewModel.kt
@@ -1,10 +1,10 @@
 package com.demo.recipelist.ui.screen
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import com.demo.recipelist.data.Recipe
 import com.demo.recipelist.data.RecipeRepository
@@ -22,10 +22,30 @@ class RecipeInsertViewModel(
     var stepUiState = mutableStateListOf("", "")
         private set
 
-    private fun validateInput(uiState: RecipeDetails = recipeUiState.recipeDetails): Boolean {
+    private fun validateInput(
+        uiState: RecipeDetails = recipeUiState.recipeDetails
+    ): Boolean {
         return with(uiState) {
-            title.isNotBlank() && description.isNotBlank() && time.isNotBlank()
+            title.isNotBlank() && time.isNotBlank()
         }
+    }
+    private fun validateIngredients(): String {
+        var ingredientsString = ""
+        for (ing in ingredientUiState) {
+            if (ing.isNotBlank()) {
+                ingredientsString = ingredientsString + ing + "\n"
+            }
+        }
+        return ingredientsString
+    }
+    private fun validateSteps(): String {
+        var stepsString = ""
+        for (step in stepUiState) {
+            if (step.isNotBlank()) {
+                stepsString = stepsString + step + "\n"
+            }
+        }
+        return stepsString
     }
     fun updateRecipeUiState(recipeDetails: RecipeDetails) {
         recipeUiState = RecipeUiState(recipeDetails = recipeDetails, isInputValid = validateInput(recipeDetails))
@@ -43,8 +63,22 @@ class RecipeInsertViewModel(
         stepUiState.remove(step)
     }
     suspend fun insertRecipe() {
-        if (validateInput()) {
-            recipeRepository.insertRecipe(recipeUiState.recipeDetails.toRecipe())
+        if (recipeUiState.recipeDetails.title.isNotBlank()) {
+            val newRecipe = recipeUiState.recipeDetails.toRecipe()
+            Log.d("InsertScreen", "old recipe: id = ${newRecipe.id}, title = ${newRecipe.title}, servings = ${newRecipe.servings}, ingredients = ${newRecipe.ingredients}, steps = ${newRecipe.steps} \n")
+            newRecipe.ingredients = validateIngredients()
+            newRecipe.steps = validateSteps()
+//            val newRecipe = Recipe(
+//                id = recipeUiState.recipeDetails.id,
+//                title = recipeUiState.recipeDetails.title,
+//                description = recipeUiState.recipeDetails.description,
+//                time = recipeUiState.recipeDetails.time,
+//                servings = recipeUiState.recipeDetails.servings.toIntOrNull() ?: 1,
+//                ingredients = validateIngredients(),
+//                steps = validateSteps()
+//            )
+            Log.d("InsertScreen", "new recipe: id = ${newRecipe.id}, title = ${newRecipe.title}, servings = ${newRecipe.servings}, ingredients = ${newRecipe.ingredients}, steps = ${newRecipe.steps} \n")
+            recipeRepository.insertRecipe(newRecipe)
         }
     }
 }
@@ -53,8 +87,10 @@ data class RecipeDetails(
     val id: Int = 0,
     val title: String = "",
     val description: String = "",
-    val time: String = "30",
-    val servings: String = "1"
+    val time: String = "",
+    val servings: String = "",
+    val ingredients: String = "",
+    val steps: String = ""
 )
 
 data class RecipeUiState(
@@ -67,7 +103,9 @@ fun RecipeDetails.toRecipe(): Recipe = Recipe(
     title = title,
     description = description,
     time = time,
-    servings = servings.toIntOrNull() ?: 1
+    servings = servings.toIntOrNull() ?: 1,
+    ingredients = ingredients,
+    steps = steps
 )
 
 fun Recipe.toRecipeDetail(): RecipeDetails = RecipeDetails(
@@ -75,7 +113,9 @@ fun Recipe.toRecipeDetail(): RecipeDetails = RecipeDetails(
     title = title,
     description = description,
     time = time,
-    servings = servings.toString()
+    servings = servings.toString(),
+    ingredients = ingredients,
+    steps = steps
 )
 
 fun Recipe.toRecipeUiState(isInputValid: Boolean = false): RecipeUiState = RecipeUiState(
